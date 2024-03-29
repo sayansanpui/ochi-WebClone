dca48b75aabb6ac38728806833a955c007646f94
Sayan Sanpui <sayansanpuicoding@gmail.com>
2024-03-30 03:17:17 +0530

    Eyes Update

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 const Eyes = () => {
     const [rotate, setRotate] = useState(0);
 
+    const [xDistance, setXDistance] = useState(0);
+    const [yDistance, setYDistance] = useState(0);
+
     useEffect(() => {
         window.addEventListener('mousemove', (e) => {
             // console.log(e.clientX, e.clientY);
@@ -13,27 +16,53 @@ const Eyes = () => {
             let deltaY = mouseY - window.innerHeight / 2;
 
             var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
-            setRotate(angle-180);
+            setRotate(angle - 180);
         })
     })
+
+    useEffect(() => {
+        window.addEventListener('mousemove', (e) => {
+            // console.log(e.clientX, e.clientY);
+            let mouseX = e.clientX;
+            let mouseY = e.clientY;
+
+            const w = window.innerWidth / 4;
+            const h = window.innerHeight / 4;
+
+            let deltaX = mouseX - window.innerWidth / 2;
+            let deltaY = mouseY - window.innerHeight / 2;
+
+            var x = (deltaX / w)*8;
+            var y = (deltaY / h)*8;
+
+            setXDistance(x);
+            setYDistance(y);
+        })
+    })
+
+    // console.log(xDistance, yDistance);
 
     return (
         <div className='eyes w-full h-screen overflow-hidden'>
             <div className='relative w-full h-full bg-cover bg-center bg-[url("Top-Viewbbcbv-1-scaled.jpg")]'>
                 <div className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] flex gap-10 '>
-                    <div className='flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100'>
-                        <div className='relative w-[8vw] h-[8vw] rounded-full bg-zinc-900 flex items-center justify-center'>
-                            <div className='text-white text-lg pFont1'>PLAY</div>
-                            <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-5">
-                                <div className='w-5 h-5 rounded-full bg-zinc-100'></div>
+                    <div className='w-[14vw] h-[14vw] rounded-full bg-zinc-100'>
+                        <div style={{ transform: `translate(${38 + xDistance}%, ${38 + yDistance}%)` }} className='absolute'>
+                            <div className='relative w-[8vw] h-[8vw] rounded-full bg-zinc-900 flex items-center justify-center'>
+                                <div className='text-white text-lg pFont1'>PLAY</div>
+                                <div style={{ transform: `translate(-50%, -50%) rotate(${rotate}deg)` }} className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-5">
+                                    <div className='w-5 h-5 rounded-full bg-zinc-100'></div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className='flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100'>
-                        <div className='relative w-[8vw] h-[8vw] rounded-full bg-zinc-900 flex items-center justify-center'>
-                            <div className='text-white text-lg pFont1'>PLAY</div>
-                            <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-5">
-                                <div className='w-5 h-5 rounded-full bg-zinc-100'></div>
+                    <div className='w-[14vw] h-[14vw] rounded-full bg-zinc-100'>
+                        <div style={{ transform: `translate(${38 + xDistance}%, ${38 + yDistance}%)` }} className='absolute'>
+                            <div className='relative w-[8vw] h-[8vw] rounded-full bg-zinc-900 flex items-center justify-center'>
+                                <div className='text-white text-lg pFont1'>PLAY</div>
+                                <div style={{ transform: `translate(-50%, -50%) rotate(${rotate}deg)` }} className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-5">
+                                    <div className='w-5 h-5 rounded-full bg-zinc-100'></div>
+                                </div>
                             </div>
                         </div>
                     </div>
